1dcf50fe9deec925e7de6d5a0103bc78a926afed
BK <39931731+BitsBytesandLife@users.noreply.github.com>
2022-01-03 18:46:04 -0600

    Add project files.

---
+++ EmployeeData/IEmployeeData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiCRUDDemo.EmployeeData
+{
+    interface IEmployeeData
+    {
+
+        List<Employee> GetEmployees();
+    }
+}
